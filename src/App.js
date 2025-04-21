0fbbcbf0ab49766f0b741552658d51a2eed0e342
Eduarda Bolgenhagen De Campos <eduardabc@ppi-multitask.com.br>
2025-04-21 18:56:17 -0300

    Add alterações

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [mensagem, setMensagem] = useState("");
+
+  const handleLogin = () => {
+    if (email === "eduardo.lino@pucpr.br" && senha === "123456") {
+      setMensagem("Acessado com sucesso!");
+    } else {
+      setMensagem("Usuário ou senha incorretos!");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Login</h1>
+        <input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+        <input type="password" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)}></input>
+        <button onClick={handleLogin}>Acessar</button>
+        <label>{mensagem}</label>
       </header>
     </div>
   );
